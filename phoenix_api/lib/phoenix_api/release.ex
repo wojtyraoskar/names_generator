9a3c031727799cfcc38728dbc326c5ccfb7df6c8
Oskar Wojtyra <oskar.wojtyra@indivi.com>
2025-07-30 17:41:11 +0200

    Satifsy workflow

--- phoenix_api/lib/phoenix_api/release.ex
+++ phoenix_api/lib/phoenix_api/release.ex
@@ -9,10 +9,11 @@ defmodule PhoenixApi.Release do
         {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
       rescue
         e in DBConnection.EncodeError ->
-          IO.puts "Migration already completed or encoding error: #{inspect(e)}"
+          IO.puts("Migration already completed or encoding error: #{inspect(e)}")
           :ok
+
         e in Postgrex.Error ->
-          IO.puts "Migration already completed or database error: #{inspect(e)}"
+          IO.puts("Migration already completed or database error: #{inspect(e)}")
           :ok
       end
     end
@@ -67,7 +68,8 @@ defmodule PhoenixApi.Release do
   end
 
   defp ensure_repo_created(repo) do
-    IO.puts "Creating #{inspect(repo)} database if it doesn't exist..."
+    IO.puts("Creating #{inspect(repo)} database if it doesn't exist...")
+
     case repo.__adapter__().storage_up(repo.config) do
       :ok -> :ok
       {:error, :already_up} -> :ok
@@ -76,7 +78,8 @@ defmodule PhoenixApi.Release do
   end
 
   defp ensure_repo_dropped(repo) do
-    IO.puts "Dropping #{inspect(repo)} database..."
+    IO.puts("Dropping #{inspect(repo)} database...")
+
     case repo.__adapter__().storage_down(repo.config) do
       :ok -> :ok
       {:error, :already_down} -> :ok
