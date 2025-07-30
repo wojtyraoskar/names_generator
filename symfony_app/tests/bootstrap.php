135f475aea1fc72ba127e13de0687e3a61e21cf6
Oskar Wojtyra <oskar.wojtyra@indivi.com>
2025-07-30 17:56:18 +0200

    Make sure that git workflow php test works

--- symfony_app/tests/bootstrap.php
+++ symfony_app/tests/bootstrap.php
@@ -11,3 +11,19 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+// Completely suppress deprecation warnings in tests
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
+// Disable Doctrine deprecation warnings completely
+if (class_exists('Doctrine\Deprecations\Deprecation')) {
+    \Doctrine\Deprecations\Deprecation::ignoreDeprecations();
+}
+
+// Set deprecation handler to null to suppress all deprecation warnings
+set_error_handler(function($severity, $message, $file, $line) {
+    if ($severity === E_DEPRECATED || $severity === E_USER_DEPRECATED) {
+        return true; // Suppress deprecation warnings
+    }
+    return false; // Let other errors through
+});
